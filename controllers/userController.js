58fe822c0c26f6566b8a83dda0e98b3bbd2c74e5
Dani <d.tarazoma@geekshubs.com>
2023-06-12 10:18:18 +0200

    feat: register with regex validations and bcrypt password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,51 @@
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
 const userController = {}
 
 userController.createUser = async (req, res) => {
 
-    try {
+    try {           
         const name = req.body.name;
         const email = req.body.email;
         const password = req.body.password;
-
         // const { name, email, password } =  req.body;
+        
+        // todo export to a utils function
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const isValidEmail = emailRegex.test(email);
 
+        if (!isValidEmail) {
+            return res.json({
+                success: true,
+                message: "Not a valid email",
+            })
+        }
+
+        // Esta expresión regular garantiza que la contraseña cumpla con los siguientes requisitos:
+        // Al menos una letra mayúscula.
+        // Al menos una letra minúscula.
+        // Al menos un número.
+        // Al menos un carácter especial (@, $, !, %, *, ?, &).
+        // Longitud entre 8 y 14 caracteres. 
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,14}$/;
+        const isValidPassword = passwordRegex.test(password)
+        if (!isValidPassword) {
+            return res.json({
+                success: true,
+                message: "Not a valid password",
+            })
+        }
+
+        const newPassword = bcrypt.hashSync(password, 8);
+
+        // todo export to a repository pattern
         const user = await User.create(
             {
                 name,
                 email: email,
-                password: password,
+                password: newPassword,
                 role: "user",
-                age: req.body.age
             }
         )
 
